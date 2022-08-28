1dc429ac00c13e0a2122389395ef97fc9a9b2c5c
Andrej132 <ruzic.ra104.2019@uns.ac.rs>
2022-08-28 22:28:47 +0200

    meni za trenera (individualni treninzi)

--- src/controller/TreningController.java
+++ src/controller/TreningController.java
@@ -74,6 +74,59 @@ public class TreningController {
 			String json = g.toJson(prikazLista, List.class);
 			return json;
 		});
+		
+		get("treninzi/trener/individualni", (req, res) -> {
+			res.type("application/json");
+			res.status(200);
+			
+			Session session = req.session(true);
+			Korisnik ulogovaniKorisnik = session.attribute("user");
+			
+			TreningService service = new TreningService();
+			List<Trening> treninzi = service.getTrenziZaKorisnika(ulogovaniKorisnik.getId());
+			List<Trening> grupniTreninzi = new ArrayList<Trening>();
+			for (Trening t : treninzi) {
+				if (t.getTip().equals(TreningTip.INDIVIDUALNI)) {
+					grupniTreninzi.add(t);
+				}
+			}
+			
+			List<TreningPrikazDTO> prikazLista = new ArrayList<TreningPrikazDTO>();
+			for (Trening t : grupniTreninzi) {
+				TreningPrikazDTO dto = new TreningPrikazDTO();
+				dto.setId(t.getId());
+				dto.setNaziv(t.getNaziv());
+				dto.setCena(t.getCena());
+				dto.setOpis(t.getOpis());
+				dto.setSlikaURL(t.getSlika());
+				dto.setTip(t.getTip().toString());
+				dto.setTrajanje(t.getTrajanje());
+				dto.setDatum(t.getDatum());
+				
+				KorisnikService ks = new KorisnikService();
+				Korisnik trener = ks.getPoId(t.getTrener());
+				if (trener != null ) {
+					dto.setTrener(trener.getIme() + " " + trener.getPrezime());
+				}
+				
+				Korisnik kupac = ks.getPoId(t.getKupac());
+				if (kupac != null) {
+					dto.setKupac(kupac.getIme() + " " + kupac.getPrezime());
+				}
+				
+				SportskiObjektiService sos = new SportskiObjektiService();
+				SportskiObjekat so = sos.getObjekatPoId(t.getSportskiObjekat());
+				if (so != null) {
+					dto.setObjekat(so.getNaziv());
+				}
+				
+				prikazLista.add(dto);
+			}
+			
+			Gson g = new GsonBuilder().setPrettyPrinting().create();
+			String json = g.toJson(prikazLista, List.class);
+			return json;
+		});
 	}
 	
 }
--- static/js/meni/trener.js
+++ static/js/meni/trener.js
@@ -12,7 +12,7 @@ function ucitajGrupneTreninge() {
 			
 		},
 	    success: function(data) {
-			
+			console.log(data)
 			treninzi = data;
 			let tableBody = $("#grupni-treninzi");
 	        tableBody.html("");
@@ -33,8 +33,43 @@ function ucitajGrupneTreninge() {
 				}
 				red += "<td><img width=50 height=50 src='" + t.slikaURL + "'</td>";				
 				tableBody.append(red);
-	        }
-			
+			}
+				
+			$.ajax({
+			    url: "/treninzi/trener/individualni",
+			    type: "GET",
+			    contentType: "application/json",
+			    dataType: "json",
+			    error: function(error) {
+					
+				},
+			    success: function(data) {
+					
+					treninzi = data;
+					let tableBody = $("#individualni-treninzi");
+			        tableBody.html("");
+			        for (let t of treninzi) {
+			            let red = "<tr>";
+						red += "<td>" + t.naziv + "</td>";
+						red += "<td>" + t.objekat + "</td>";
+						red += "<td>" + t.tip + "</td>";
+						red += "<td>" + t.trajanje + "</td>";
+						red += "<td>" + t.opis + "</td>";
+						red += "<td>" + t.cena + "</td>";
+						red += "<td>" + t.datum + "</td>";
+						red += "<td>" + t.trener + "</td>";
+						if (t.kupac) {
+							red += "<td>" + t.kupac + "</td>";		
+						} else {
+							red += "<td>/</td>";
+						}
+						red += "<td><img width=50 height=50 src='" + t.slikaURL + "'</td>";
+						red += "<td><a href='' class='btn btn-danger'>Otkazivanje</a></td>";				
+						tableBody.append(red);
+			        }
+					
+			    }
+			});
 	    }
 	});
 }
