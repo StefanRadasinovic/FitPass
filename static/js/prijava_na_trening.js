0a4e1edaf6145fef9bd0a7ff490875608f76fdf1
Andrej132 <ruzic.ra104.2019@uns.ac.rs>
2022-09-01 07:07:17 +0200

    prijavljivanje na trening

---
+++ static/js/prijava_na_trening.js
@@ -0,0 +1,69 @@
+$(document).ready( function() {
+	getSportskiObjekti();
+});
+
+
+function getSportskiObjekti() {
+	$.ajax({
+        url: "sportski-objekti",
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        complete: function(data) {
+            const sportskiObjekti = data.responseJSON;
+            
+            let elem = $("#objekti");
+            elem.html("");
+            for (let s of sportskiObjekti) {
+				let red = "<tr>"
+				red += "<td>" + s.naziv + "</td>";
+				red += "<td>" + s.tipObjekta + "</td>";
+				red += "<td>" + s.status + "</td>";
+				red += "<td>" + s.sadrzaj + "</td>";
+				red += "<td>" + s.lokacija.adresa + "</td>";
+				red += "<td>" + s.radnoVreme + "</td>";
+				red += "<td>" + s.prosecnaOcena + "</td>";
+				red += "<td><img width=50 height=50 src='" + s.logo + "'</td>";
+				red += `<td><button class="btn btn-dark" onclick="ucitajTreningeZaObjekat('${s.id}')">Treninzi</button></td>`
+				red += "</tr>";
+				
+                elem.append(red);
+            }
+        }
+	});
+}
+
+function ucitajTreningeZaObjekat(objekatId) {
+	$.ajax({
+	    url: "/treninzi/objekat/" + objekatId,
+	    type: "GET",
+	    contentType: "application/json",
+	    dataType: "json",
+	    error: function(error) {
+			alert("Treninzi nisu ucitani")
+		},
+	    success: function(data) {
+			treninzi = data;
+			let tableBody = $("#treninzi");
+	        tableBody.html("");
+	        for (let t of treninzi) {
+	            let red = "<tr>";
+				red += "<td>" + t.naziv + "</td>";
+				red += "<td>" + t.objekat + "</td>";
+				red += "<td>" + t.status + "</td>";
+				red += "<td>" + t.tip + "</td>";
+				red += "<td>" + t.trajanje + "</td>";
+				red += "<td>" + t.opis + "</td>";
+				red += "<td>" + t.cena + "</td>";
+				red += "<td>" + t.datum + "</td>";
+				red += "<td>" + t.trener + "</td>";	
+				red += "<td>" + t.kupac + "</td>";						
+				red += "<td><img width=50 height=50 src='" + t.slikaURL + "'</td>";			
+				red += `<td><button class="btn btn-dark" onclick="prijava('${t.id}')">Prijava</button></td>`;				
+				tableBody.append(red);
+			
+	    }
+	}
+	});
+	
+}
