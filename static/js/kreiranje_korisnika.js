e5da6d0988d60ffdef0f0949d8bb7730d1cf25e3
StefanRadasinovic <stefanstele00@gmail.com>
2022-09-01 06:36:16 +0200

    Registracija menadzera i trenera

---
+++ static/js/kreiranje_korisnika.js
@@ -0,0 +1,33 @@
+function getFormData($form) {
+	let unindexedArray = $form.serializeArray();
+	let indexedArray = {};
+	
+	$.map(unindexedArray, function(n, i){
+		indexedArray[n['name']] = n['value'];
+    });
+
+    return indexedArray;
+}
+
+function registruj(){
+    var data = getFormData($("#registracija"));
+    var s = JSON.stringify(data);
+    var myURL = "";
+    myURL = "registracija";
+        
+    $.ajax({
+        url: myURL,
+        type: "POST",
+        data: s,
+        contentType: "application/json",
+        dataType: "json",
+        complete : function (data) {
+            if(data.status == 400)
+                alert("Neuspesna registracija");
+            else{
+                alert("Uspesno ste registrovali korisnika.");
+                window.location.replace("/");
+            }
+        }
+    });
+}
